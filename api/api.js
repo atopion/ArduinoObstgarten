055c13beffa66f3bf2b01f335000f9823e07f231
relawe <r.wetzelt@yahoo.de>
2020-01-23 22:37:12 +0100

    moved next() again because of stupidity before

--- api/api.js
+++ api/api.js
@@ -184,8 +184,8 @@ function postNodes(req, res, next) {
         console.info("Method", req.method);
 
         if(req.method === "GET" && session_found === true) {
-            redis_connector.get(req_username).then(val => res.status(200).send(val));
-            next();
+            redis_connector.get(req_username).then(val => res.status(200).send(val)).then(next());
+        
         }
         else if(req.method === "POST" && session_found === true) {
             var post = req.body;
